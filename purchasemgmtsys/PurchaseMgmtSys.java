6f99720e7a4eb175f7486fcbb6f00e63ca6b8f1c
guptakshitij <30634796+guptakshitij@users.noreply.github.com>
2018-03-25 17:00:27 +0530

    Add files via upload

---
+++ purchasemgmtsys/PurchaseMgmtSys.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package purchasemgmtsys;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Swarnim
+ */
+public class PurchaseMgmtSys {
+public PurchaseMgmtSys()
+{
+            JOptionPane.showMessageDialog(null,"Welcome to Purchase management system of SRM");
+        new Home().setVisible(true);
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
